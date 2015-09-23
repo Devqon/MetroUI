9fc84cf1914c7515e3e5fd878df5fe6e0dc7d396
Devqon <s.geerts@live.nl>
2015-09-23 16:34:22 +0200

    First commit

---
+++ Handlers/MetroThemeSettingsPartHandler.cs
@@ -0,0 +1,27 @@
+﻿using MetroUI.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Localization;
+
+namespace MetroUI.Handlers
+{
+    public class MetroThemeSettingsPartHandler : ContentHandler
+    {
+        public MetroThemeSettingsPartHandler() {
+            T = NullLocalizer.Instance;
+            Filters.Add(new ActivatingFilter<MetroThemeSettingsPart>("Site"));
+            Filters.Add(new TemplateFilterForPart<MetroThemeSettingsPart>("MetroThemeSettings", "Parts/MetroThemeSettings", "MetroUI"));
+        }
+
+        public Localizer T { get; set; }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context) {
+            if (context.ContentItem.ContentType != "Site")
+                return;
+
+            base.GetItemMetadata(context);
+
+            context.Metadata.EditorGroupInfo.Add(new GroupInfo(T("MetroUI")));
+        }
+    }
+}
---
+++ Models/MetroThemeSettingsPart.cs
@@ -0,0 +1,32 @@
+﻿using Orchard.ContentManagement;
+
+namespace MetroUI.Models
+{
+    public class MetroThemeSettingsPart : ContentPart
+    {
+        public string NavigationColor {
+            get { return this.Retrieve(r => r.NavigationColor); }
+            set { this.Store(r => r.NavigationColor, value); }
+        }
+
+        public bool FixedHeader {
+            get { return this.Retrieve(r => r.FixedHeader); }
+            set { this.Store(r => r.FixedHeader, value); }
+        }
+
+        public bool DarkBreadcrumbs {
+            get { return this.Retrieve(r => r.DarkBreadcrumbs); }
+            set { this.Store(r => r.DarkBreadcrumbs, value); }
+        }
+
+        public string BreadcrumbSize {
+            get { return this.Retrieve(r => r.BreadcrumbSize); }
+            set { this.Store(r => r.BreadcrumbSize, value); }
+        }
+
+        public string BreadcrumbMode {
+            get { return this.Retrieve(r => r.BreadcrumbMode); }
+            set { this.Store(r => r.BreadcrumbMode, value); }
+        }
+    }
+}
