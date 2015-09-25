2d4fa91ad368f66454c42ea3144286bdb7b79684
Devqon <s.geerts@live.nl>
2015-09-25 11:13:08 +0200

    jqcloud, account pages

---
+++ ResourceManifest.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.UI.Resources;
+
+namespace MetroUI
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest
+                .DefineScript("jQCloud")
+                .SetUrl("jqcloud/jqcloud.min.js", "jqcloud/jqcloud.js")
+                .SetDependencies("jQuery");
+
+            manifest
+                .DefineStyle("jQCloud")
+                .SetUrl("jqcloud/jqcloud.min.css", "jqcloud/jqcloud.css");
+        }
+    }
+}
---
+++ Scripts/custom.js
@@ -0,0 +1,1 @@
+﻿// any custom js
