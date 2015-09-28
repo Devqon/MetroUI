120b87d97f2ffba144d0d5b2d99bca957838a6a8
Devqon <s.geerts@live.nl>
2015-09-28 14:31:51 +0200

    menu modes

---
+++ MenuAlternatesFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Orchard.Core.Navigation.Models;
+using Orchard.DisplayManagement.Implementation;
+
+namespace MetroUI
+{
+    public class MenuAlternatesFactory : ShapeDisplayEvents
+    {
+        public override void Displaying(ShapeDisplayingContext context) {
+            context.ShapeMetadata.OnDisplaying(displayedContext => {
+
+                var contentPart = displayedContext.Shape.ContentPart;
+                if (contentPart == null) return;
+
+                var menuWidgetPart = contentPart as MenuWidgetPart;
+                if (menuWidgetPart == null) return;
+
+                var menuModeField = menuWidgetPart.Fields.FirstOrDefault(f => f.Name == "MenuMode" && f.FieldDefinition.Name == "EnumerationField");
+                if (menuModeField == null) return;
+
+                var menuSizeField = menuWidgetPart.Fields.FirstOrDefault(f => f.Name == "MenuSize");
+                string menuSize = menuSizeField != null ? ((dynamic) menuSizeField).Value : string.Empty;
+
+                string menuMode = ((dynamic) menuModeField).Value;
+                if (!string.IsNullOrEmpty(menuMode)) {
+                    displayedContext.ShapeMetadata.Alternates.Add(
+                        // MenuWidget_{MenuMode}
+                        string.Format("{0}__{1}", displayedContext.ShapeMetadata.Type, menuMode));
+                }
+                if (!string.IsNullOrEmpty(menuSize)) {
+                    displayedContext.Shape.MenuSize = menuSize;
+                }
+            });
+        }
+    }
+}
---
+++ Migrations.cs
@@ -0,0 +1,30 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Navigation.Models;
+using Orchard.Data.Migration;
+
+namespace MetroUI
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create() {
+
+            ContentDefinitionManager.AlterPartDefinition(typeof (MenuWidgetPart).Name, part => part
+                .WithField("MenuMode", field => field
+                    .WithDisplayName("Menu Mode")
+                    .OfType("EnumerationField")
+                    .WithSetting("EnumerationFieldSettings.Options", 
+                        string.Join(System.Environment.NewLine,
+                            new [] {"Default", "Vertical", "MMenu", "FMenu", "HMenu", "TMenu", "VMenu", "DMenu"}))
+                    .WithSetting("EnumerationFieldSettings.Hint", string.Format("The menu mode, see {0} for the available modes", "<a href=\"http://metroui.org.ua/menu.html\">menu modes</a>")))
+                            
+                .WithField("MenuSize", field => field
+                    .WithDisplayName("Menu Size")
+                    .OfType("EnumerationField")
+                    .WithSetting("EnumerationFieldSettings.Options",
+                        string.Join(System.Environment.NewLine,
+                            new [] {"Normal", "Compact"}))));
+
+            return 1;
+        }
+    }
+}
